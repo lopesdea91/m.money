462e6386aef3244df6896aa074267cd73f8530c0
Anderson Lopes <lopesdea91@gmail.com>
2025-07-21 21:15:14 -0300

    add tailwind and components shadcn

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 
+import "./index.css";
+
 import Routes from "./routes.tsx";
 
 createRoot(document.getElementById("root")!).render(
